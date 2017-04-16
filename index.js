10591a5c46bd7e490a90f3683122ae30f8bf752b
naoey <naoey20@gmail.com>
2017-04-16 09:48:22 +0530

    Seems to be working fine now? ¯\_(ツ)_ /¯
    - Glob for files instead of searching stupidly
    - Store results and errors separately - callback results in both cases, err only when breakOnFailure
    - Move one more up the chain and run tests on Webpack 'run' hook now
    - Bump package version

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
-import fs from 'fs';
 import path from 'path';
+import glob from 'glob';
+
 import Mocha from 'mocha';
 
 class WebpackMochaPlugin {
   constructor(options) {
     this.options = options;
-
-    const testDir = options.testDir ? options.testDir : 'test';
-    const pattern = new RegExp(options.pattern ? options.pattern : '.js');
-
-    this.mocha = new Mocha();
-
-    fs.readdirSync(testDir)
-    .filter(file => pattern.test(file))
-    .forEach(file => this.mocha.addFile(path.join(options.testDir, file)));
+    this.pattern = options.pattern ? options.pattern : './test/**/*.js';
 
     this.apply = this.apply.bind(this);
-    this.handleFailure = this.handleFailure.bind(this);
-  }
 
-  handleFailure(compilation, test, err) {
-    const error = new Error(`test ${test.title}\n${JSON.stringify(err)}`);
-    if (this.options.emitWarn) {
-      compilation.warnings.push(error);
-    } else {
-      compilation.errors.push(error);
-    }
+    this.error = null;
   }
 
   apply(compiler) {
-    compiler.plugin('after-compile', (compilation, callback) => {
+    const mocha = new Mocha();
+
+    const results = {};
+
+    compiler.plugin('run', (c, callback) => {
       if (this.options.setupFile) {
-        require(path.resolve(compilation.options.context, this.options.setupFile)); // eslint-disable-line
+        require(path.resolve(compiler.options.context, this.options.setupFile)); // eslint-disable-line
       }
 
-      this.mocha.run()
-      .on('fail', (test, err) => this.handleFailure(compilation, test, err))
-      .on('end', callback);
+      glob(this.pattern, (globError, files) => {
+        files
+        .map(file => path.resolve(compiler.options.context, file))
+        .forEach(file => mocha.addFile(file));
+
+        mocha.run()
+        .on('test', (test) => { results[test.title] = null; })
+        .on('pass', (test) => { results[test.title] = 'pass'; })
+        .on('fail', (test, err) => {
+          if (!this.errors) {
+            this.errors = [];
+          }
+
+          results[test.title] = 'fail';
+          this.errors.push(new Error(`test ${test.title}\n${JSON.stringify(err)}`));
+        })
+        .on('end', () => {
+          if (this.options.breakOnFailure) {
+            callback(this.errors, results);
+          } else {
+            callback(null, results);
+          }
+        });
+      });
     });
   }
 }
